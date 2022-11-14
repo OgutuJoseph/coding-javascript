6f06a138a87960507f9becf34e01390c542a5623
OgutuJoseph <onyango.joseph@yahoo.com>
2022-11-14 15:24:01 +0300

    Arrays-copyWithin

--- controllers/arrays.js
+++ controllers/arrays.js
@@ -2,24 +2,24 @@
 
 // concat()
 const concat = async (req, res, next) => {
-    /** join two arrays */
+    /** Join two arrays */
     // const arr1 = ["Cecilie", "Lone"];
     // const arr2 = ["Emil", "Tobias", "Linus"];
     // const children = arr1.concat(arr2); 
 
 
-    /** join three arrays */
+    /** Join three arrays */
     // const arr1 = ["Cecilie", "Lone"];
     // const arr2 = ["Emil", "Tobias", "Linus"];
     // const arr3 = ["Robin"];
     // const children = arr1.concat(arr2, arr3);
 
-    /** concatenate string and numbers */
+    /** Concatenate string and numbers */
     // const arr1 = ["Cecilie", "Lone"];
     // const arr2 = [1, 2, 3];
     // const arr3 = arr1.concat(arr2);
 
-    /** concatenate nested arrays */
+    /** Concatenate nested arrays */
     const arr1 = [1, 2, [3, 4]];
     const arr2 = [[5, 6], 7, 8];
     const arr3 = arr1.concat(arr2);
@@ -36,7 +36,29 @@ const constructor = async (req, res, next) => {
     // res.status(200).json(text);
 };
 
+// copyWithin()
+const copyWithin = async (req, res, next) => { 
+    /** Copy the first two array elements to the last two array elements */
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    // fruits.copyWithin(2, 0);
+
+    /** Copy the first two array elements to the last two array elements */
+    // const fruits = ["Banana", "Orange", "Apple", "Mango", "fruit5", "fruit6"];
+    // fruits.copyWithin(4, 0);
+
+    /** Copy the first two array elements to the third and fourth position */
+    // const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+    // fruits.copyWithin(2, 0, 2);
+
+    /** Copy the first three array elements to the fifth, sixth and seventh */
+    const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "fruit6", "fruit7"];
+    fruits.copyWithin(4, 0, 3);
+
+    res.status(200).json(fruits);
+};
+
 module.exports = {
     concat,
-    constructor
+    constructor,
+    copyWithin
 };
--- routes/arrays.js
+++ routes/arrays.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const { concat, constructor } = require('../controllers/arrays');
+const { 
+    concat, 
+    constructor,
+    copyWithin
+} = require('../controllers/arrays');
 
 // concat()
 router.get('/concat', concat);
 router.get('/constructor', constructor);
+router.get('/copyWithin', copyWithin);
 
 module.exports = router;
